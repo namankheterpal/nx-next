7e3a2ae34db4c7e319f8c07e027aa0d4318dfe26
Naman Kheterpal <naman.kheterpal@leaseplan.com>
2023-08-01 19:59:05 +0200

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,53 @@
+console.log("here...........................")
 const babelConfig = {
-    presets: [
+    "presets": [
         [
-            '@babel/preset-env',
+            "@babel/preset-env",
             {
-                targets: {
-                    node: 'current',
-                    browsers: ['>0.25%', 'not ie 11', 'not op_mini all'],
+                "targets": {
+                    "node": "current",
+                    "browsers": [
+                        ">0.25%",
+                        "not ie 11",
+                        "not op_mini all"
+                    ],
                 },
             },
         ],
-        '@babel/preset-react',
-        '@babel/preset-typescript',
+        "next/babel",
     ],
-    plugins: [
-        ['@babel/plugin-proposal-class-properties', { loose: true }],
-        ['@babel/plugin-proposal-private-methods', { loose: true }],
-        ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-proposal-object-rest-spread',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-export-namespace-from',
-        'babel-plugin-ramda',
+    "plugins": [
         [
-            'babel-plugin-styled-components',
-            { ssr: true, displayName: true, preprocess: false },
+            "@babel/plugin-proposal-class-properties",
+            {
+                "loose": true
+            }
+        ],
+        [
+            "@babel/plugin-proposal-private-methods",
+            {
+                "loose": true
+            }
+        ],
+        [
+            "@babel/plugin-proposal-private-property-in-object",
+            {
+                "loose": true
+            }
+        ],
+        "@babel/plugin-transform-runtime",
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-proposal-nullish-coalescing-operator",
+        "@babel/plugin-proposal-export-namespace-from",
+        "babel-plugin-ramda",
+        [
+            "babel-plugin-styled-components",
+            {
+                "ssr": true,
+                "displayName": true,
+                "preprocess": false
+            },
         ],
     ],
 }
